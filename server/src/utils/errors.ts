17732af8f7e6f067453c2686f2b3d68f0fce28e4
zuka-yuma <yuito.2742@gmail.com>
2026-04-30 01:25:40 +0900

    feat: add AppError base class and error subclasses

---
+++ server/src/utils/errors.ts
@@ -0,0 +1,26 @@
+export class AppError extends Error {
+    public statuscode: number
+
+    constructor(statuscode: number, message: string) {
+        super(message)
+        this.statuscode = statuscode
+    }
+}
+
+export class UnauthorizedError extends AppError {
+    constructor() {
+        super(401, "Unauthorized")
+    }
+}
+
+export class NotFoundError extends AppError {
+    constructor() {
+        super(404, "Not Found")
+    }
+}
+
+export class ValidationError extends AppError {
+    constructor() {
+        super(422, "Validation Error")
+    }
+}
