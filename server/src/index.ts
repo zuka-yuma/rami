db8f84d7e7c40d79437dfe33c7c420eca57136c0
zuka-yuma <yuito.2742@gmail.com>
2026-04-30 01:26:08 +0900

    feat: add global error handler for AppError

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,17 @@ import fastify from 'fastify'
 import cors from '@fastify/cors'
 import cookie from '@fastify/cookie'
 import { config } from './config.js'
+import { AppError } from './utils/errors.js'
 
 const server = fastify()
+
+server.setErrorHandler((error, request, reply) => {
+    if (error instanceof AppError) {
+        reply.status(error.statuscode).send({ status:error.message, timeStamp: Date.now()})
+    } else {
+        reply.status(500).send({ status:"Server Error", timeStamp: Date.now()})
+    }
+})
 
 server.register(cors, {
     origin: 'http://localhost:5173',
