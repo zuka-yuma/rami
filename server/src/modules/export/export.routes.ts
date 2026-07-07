49b7b86e6d0dca5e758dbb6964812d359266db8b
zuka-yuma <yuito.2742@gmail.com>
2026-07-07 19:34:43 +0900

    feat: add CSV export

--- server/src/modules/export/export.routes.ts
+++ server/src/modules/export/export.routes.ts
@@ -2,15 +2,25 @@ import type { FastifyInstance, FastifyPluginOptions } from 'fastify'
 // import { UnauthorizedError } from '../../utils/errors.js'
 // import { isProduction } from '../../config.js'
 import { authenticate } from '../../plugins/authenticate.js'
-import { exportJSON } from './export.service.js'
+import { exportCSV, exportJSON } from './export.service.js'
 
 export const exportRoutes = async (fastify: FastifyInstance) => {
     fastify.get('/json', { preHandler: [authenticate] },
         async (request, reply) => {
             const userId = request.user.userId
             reply.header('Content-Disposition', 'attachment; filename="rami.json"')
-            const tree = await exportJSON(userId)
-            reply.status(200).send(tree)
+            const json = await exportJSON(userId)
+            reply.status(200).send(json)
+        }
+    )
+
+    fastify.get('/csv', { preHandler: [authenticate]},
+        async (request, reply) => {
+            const userId = request.user.userId
+            reply.header('Content-Disposition', 'attachment; filename="rami.csv"')
+            reply.type('text/csv')
+            const csv = await exportCSV(userId)
+            reply.status(200).send(csv)
         }
     )
 }
